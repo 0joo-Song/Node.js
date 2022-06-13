48221372a6e6c933262a372456bcf9b38cacd54f
0joo-Song <syoung7024@gmail.com>
2022-06-13 18:21:24 +0900

    DB 셋팅

--- routes/page.js
+++ routes/page.js
@@ -3,26 +3,27 @@ const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
-    res.locals.user = null;
-    res.locals.followerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followerIdList = [];
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
 });
 
 router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내 정보 - NodeBird' });
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
 router.get('/join', (req, res) => {
-    res.render('join', { title : '회원가입 - NodeBird' });
+  res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res) => {
-    const twits = [];
-    res.render('main', {
-        title: 'NodeBird',
-        twits
-    })
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
 });
 
 module.exports = router;
